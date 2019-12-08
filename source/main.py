2ea946441f51687d91bcecf65205b0e38635843b
jvcanavarro <jvcanavarro@gmail.com>
2019-12-08 11:36:33 -0300

    Refactoring

--- source/main.py
+++ source/main.py
@@ -1,25 +1,55 @@
-from login import LoginScreen
-from lines import LinesScreen
-from system import SystemScreen
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-Builder.load_file('system.kv')
-Builder.load_file('lines.kv')
-Builder.load_file('login.kv')
+class CreateAccountScreen(Screen):
+    pass
 
-sm = ScreenManager()
-sm.add_widget(LoginScreen(name='login'))
-sm.add_widget(LinesScreen(name='lines'))
-sm.add_widget(SystemScreen(name='system'))
+
+class LoginScreen(Screen):
+    def validate_user(self):
+        username = self.ids.username_field
+        password = self.ids.password_field
+        info = self.ids.info
+
+        if username.text == '' or password == '':
+            info.text = '[color=#FF0000]Username and Password required[/color]'
+        else:
+            self.manager.current = 'lines'
+
+
+class LinesScreen(Screen):
+    pass
+
+
+class SystemScreen(Screen):
+    def add_route(self):
+        route = BoxLayout(size_hint_y=None, height=30, spacing=5)
+
+        for i in range(10):
+            route.add_widget(TextInput())
+
+        container = self.ids.travels
+        container.add_widget(travel)
 
 
 class ManagerApp(App):
-
     def build(self):
-        return sm
+        Builder.load_file('design/system.kv')
+        Builder.load_file('design/lines.kv')
+        Builder.load_file('design/createaccount.kv')
+        Builder.load_file('design/login.kv')
+
+        manager = ScreenManager()
+
+        manager.add_widget(LoginScreen(name='login'))
+        manager.add_widget(CreateAccountScreen(name='create'))
+        manager.add_widget(LinesScreen(name='lines'))
+        manager.add_widget(SystemScreen(name='system'))
+        return manager
 
 
 if __name__ == '__main__':
