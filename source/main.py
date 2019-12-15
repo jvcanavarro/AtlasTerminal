9f344d31e7e374c9b544bc8e63294f706f56176d
jvcanavarro <jvcanavarro@gmail.com>
2019-12-15 16:16:33 -0300

    Missing System Logic

--- source/main.py
+++ source/main.py
@@ -6,23 +6,64 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 class CreateAccountScreen(Screen):
-    pass
+    def create_user(self):
+        login = self.manager.get_screen('login')
+        accounts = login.accounts
+        emails = login.emails
+
+        info = self.ids.info
+        username = self.ids.username
+        email = self.ids.email
+        password = self.ids.password
+        conf_password = self.ids.conf_password
+
+
+        if username.text and email.text and password.text and conf_password.text:
+            if password.text != conf_password.text:
+                info.text = 'Senhas diferentes'
+
+            elif (username.text, password.text) not in accounts.items() and email not in emails:
+                accounts[username.text] = password.text
+                emails[username.text] = email.text
+                info.text = 'Conta criada'
+
+            else:
+                info.text = 'Conta já existente'
+
+        else:
+            info.text = 'Campos faltando'
 
 
 class ForgotPasswordScreen(Screen):
-    pass
+    def send_recovery_email(self):
+        email = self.ids.recovery_email
+        info = self.ids.info
+        login = self.manager.get_screen('login')
+        emails = login.emails
+        if email.text in emails.values():
+            info.text = 'Email enviado'
+        else:
+            info.text = 'Email não registrado'
 
 
 class LoginScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.accounts = {'admin': 'admin'}
+        self.emails = {'admin': 'admin@admin.com'}
+
     def validate_user(self):
         username = self.ids.username_field
         password = self.ids.password_field
         info = self.ids.info
 
-        if username.text == '' or password == '':
-            info.text = '[color=#FF0000]Username and Password required[/color]'
-        else:
+        if (username.text, password.text) in self.accounts.items():
             self.manager.current = 'lines'
+            username.text = ''
+            password.text = ''
+        else:
+            print(self.accounts)
+            info.text = '[color=#FF0000]Usuário não Registrado ou Senha Incorreta[/color]'
 
 
 class LinesScreen(Screen):
@@ -41,6 +82,7 @@ class SystemScreen(Screen):
 
 
 class ManagerApp(App):
+
     def build(self):
         Builder.load_file('design/system.kv')
         Builder.load_file('design/lines.kv')
